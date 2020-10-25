2dccfe156c07661eb8c1f5c908aa6271cf87d6a6
Leman316 <56719469+Leman316@users.noreply.github.com>
2020-10-25 21:38:14 +0600

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
+
+
 }
 
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(home: Text('FPS'),);
+    throw UnimplementedError();
+  }
+
+
+}
 
