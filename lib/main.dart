ef084fdca52c8d9be078e785064f7076ac6f0337
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-02-05 14:16:31 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'drawer.dart';
+import 'data.dart';
+import 'UI_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,11 +18,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _state = true;
+
+  void _change() {
+    setState(() {
+      _state = !_state;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('UI Practice')),
+        drawer: AppDrawer(),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                child: Text('Press Button'),
+                onPressed: _change,
+              ),
+              _state ? Text('Home') : Text('Away'),
+              SizedBox(
+                height: 150,
+              ),
+            ],
+          ),
+        ),
+      ),
+      routes: {
+        UIScreen.routename: (ctx) => UIScreen(),
+      },
+    );
 
     // throw UnimplementedError();
   }
