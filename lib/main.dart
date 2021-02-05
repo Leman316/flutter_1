c31c05ed054813ca3439f325066a593ac74f08c2
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-02-05 12:54:47 +0600

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './Question.dart';
-import './Answer.dart';
-import './quiz.dart';
-import './result.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -20,80 +15,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _quesIndex = 0;
-  int _totalscore = 0;
-
-  static const ques = [
-    {
-      'question': 'The Color?',
-      'answers': [
-        {'text': 'Black', 'score': 6},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 2},
-      ],
-    },
-    {
-      'question': 'The Animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 5},
-        {'text': 'Dog', 'score': 1},
-        {'text': 'Fox', 'score': 3},
-      ],
-    },
-    {
-      'question': 'The Shape?',
-      'answers': [
-        {'text': 'Rectangle', 'score': 2},
-        {'text': 'Square', 'score': 4},
-        {'text': 'Circle', 'score': 8},
-      ],
-    },
-  ];
-
-  void _reset(){
-    setState(() {
-      _quesIndex=0;
-      _totalscore=0;
-    });
-
-  }
-
-  void _answer(int score) {
-    if (_quesIndex < ques.length) {
-      print("No questions left");
-    }
-
-    _totalscore += score;
-    print(_totalscore);
-
-    setState(() {
-      _quesIndex = _quesIndex + 1;
-    });
-
-    print(_quesIndex);
-    // print('Answer');
-  }
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Test'),
-        ),
-        body: _quesIndex < ques.length
-            ? Quiz(
-                question: ques,
-                answer: _answer,
-                index: _quesIndex,
-              )
-            : Result(_totalscore,_reset),
-      ),
-    );
+    return MaterialApp(home: Scaffold());
 
-    throw UnimplementedError();
+    // throw UnimplementedError();
   }
 }
